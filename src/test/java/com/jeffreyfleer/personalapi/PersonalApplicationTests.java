ba5b50ae9cabf1d3ce72b870d1f1ac96ffb7bf9c
Jeffrey Fleer <jeffreyfleer+github@gmail.com>
2024-04-02 19:19:56 -0600

    boiler plate

---
+++ src/test/java/com/jeffreyfleer/personalapi/PersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffreyfleer.personalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
